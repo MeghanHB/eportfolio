8007ef1a42573ee459e0467f4772efbf56b1923a
Meghan Hoffman <meghanhoffman@Meghans-Laptop.local>
2026-05-30 15:27:30 -0400

    working

---
+++ app.js
@@ -0,0 +1,26 @@
+// template_x87ekvm
+// service_60nvk4l
+// -Trnv2qDy8CJGhv8u
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
+success.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_60nvk4l',
+            'template_x87ekvm',
+            event.target,
+            '-Trnv2qDy8CJGhv8u'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable.  Please contact me directly on meghanshoffman@gmail.com"
+            );
+        })
+} 
+
