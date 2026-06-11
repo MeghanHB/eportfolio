ae478dec7c1bc7bf4a637390d2fdb27048ba4bcf
Meghan Hoffman <meghanhoffman@Meghans-Laptop.local>
2026-06-11 10:20:24 -0400

    new email address, still not working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// template_x87ekvm
-// service_60nvk4l
-// gpu7iywtk26Lw_n5G
+// template_z2kwtgc
+// service_h4ys1if
+// rsHLmSV7wPXyb9hIP
 
 function toggleModal() {
     document.body.classList.toggle("modal--open");
@@ -15,8 +15,8 @@ const success = document.querySelector('.modal__overlay--success')
 loading.classList += " modal__overlay--visible"
     emailjs
         .sendForm( 
-            'service_60nvk4l',
-            'template_x87ekvm',
+            'service_h4ys1if',
+            'template_z2kwtgc',
             event.target,
             'gpu7iywtk26Lw_n5G'
         ).then(() => {
@@ -25,7 +25,7 @@ loading.classList += " modal__overlay--visible"
         }).catch(() => {
             loading.classList.remove("modal__overlay--visible");
             alert(
-                "The email service is temporarily unavailable.  Please contact me directly on meghanshoffman@gmail.com"
+                "The email service is temporarily unavailable.  Please contact me directly on meghanhb31@gmail.com"
             );
         })
 } 
