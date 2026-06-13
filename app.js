a75678905b1d9f0aebbc2a9c872622d8c5a03717
Meghan Hoffman <meghanhoffman@Meghans-Laptop.local>
2026-06-13 15:15:10 -0400

    finalized structure portfolio

--- app.js
+++ app.js
@@ -2,10 +2,38 @@
 // service_h4ys1if
 // rsHLmSV7wPXyb9hIP
 
+
+let contrastToggle = false
+const scaleFactor = 1/20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+    
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+
+
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+    document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function toggleModal() {
     document.body.classList.toggle("modal--open");
         document.body.classList.toggle(".modal--open");
-
 }
 
 function contact(event) {
@@ -18,7 +46,7 @@ loading.classList += " modal__overlay--visible"
             'service_h4ys1if',
             'template_z2kwtgc',
             event.target,
-            'gpu7iywtk26Lw_n5G'
+            'rsHLmSV7wPXyb9hIP'
         ).then(() => {
             loading.classList.remove("modal__overlay--visible")
         success.classList += " modal__overlay--visible"
